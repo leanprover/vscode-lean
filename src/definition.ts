6d025025ee6b2f1ebc7306e4a64af57e77a13410
Gabriel Ebner <gebner@gebner.org>
2022-05-20 16:21:29 +0200

    fix: resolve position in correct document

--- src/definition.ts
+++ src/definition.ts
@@ -1,4 +1,4 @@
-import { Definition, DefinitionProvider, Location, Position, TextDocument, Uri } from 'vscode';
+import { Definition, DefinitionProvider, Location, Position, TextDocument, Uri, workspace } from 'vscode';
 import { Server } from './server';
 import { CodePointPosition } from './utils/utf16Position';
 
@@ -16,7 +16,7 @@ export class LeanDefinitionProvider implements DefinitionProvider {
             const src = response.record.source;
             const uri = src.file ? Uri.file(src.file) : document.uri;
             const pos = new CodePointPosition(src.line - 1, src.column);
-            return new Location(uri, pos.toPosition(document));
+            return new Location(uri, pos.toPosition(await workspace.openTextDocument(uri)));
         } else {
             return null;
         }
